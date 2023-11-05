e03075c570eb4ca0330afba7b7c7adcb83e91b37
Ivan <ivan.mondragon.sotres@gmail.com>
2023-11-05 16:36:14 -0600

    (ADD) Added angular material
    Command from official doc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SalesOrderModule } from './sales-order/sales-order-management.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { SalesOrderModule } from './sales-order/sales-order-management.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SalesOrderModule
+    SalesOrderModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
