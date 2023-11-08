e26eae757b4c911806a5bd808648bcabcc525972
Ivan <ivan.mondragon.sotres@gmail.com>
2023-11-08 01:38:30 -0600

    (ADD) Added tests that need data mandatory for its basic creation
    All tests pass with ng test

--- src/app/sales-order/components/card/card.component.spec.ts
+++ src/app/sales-order/components/card/card.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardComponent } from './card.component';
+import { MaterialModule } from 'src/app/material/material.module';
+import { ItemTableModule } from '../item-table/item-table.module';
 
 describe('CardComponent', () => {
   let component: CardComponent;
@@ -8,14 +10,32 @@ describe('CardComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CardComponent]
+      imports: [MaterialModule, ItemTableModule],
+      declarations: [CardComponent],
     });
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    component.order = {
+      id: 'ID_SO63',
+      customer: 'Modelo',
+      items: [
+        {
+          name: 'negra modelo',
+          price: 100,
+          quantity: 10,
+        },
+      ],
+      creationDate: new Date('2023-11-06T13:47:05.892Z'),
+      subtotal: 1000,
+      vat: 0.16,
+      total: 1160,
+      isCancel: true,
+      cancellationDate: new Date('2023-11-06T14:12:58.772Z'),
+    };
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 });
--- src/app/sales-order/components/dialog-card/dialog-card.component.spec.ts
+++ src/app/sales-order/components/dialog-card/dialog-card.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DialogCardComponent } from './dialog-card.component';
+import { MaterialModule } from 'src/app/material/material.module';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CardComponent } from '../card/card.component';
+import { ItemTableModule } from '../item-table/item-table.module';
 
 describe('DialogCardComponent', () => {
   let component: DialogCardComponent;
@@ -8,14 +12,35 @@ describe('DialogCardComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [DialogCardComponent]
+      imports: [MaterialModule, ItemTableModule],
+      declarations: [DialogCardComponent, CardComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: undefined },
+      ],
     });
     fixture = TestBed.createComponent(DialogCardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    component.data = {
+      id: 'ID_SO63',
+      customer: 'Modelo',
+      items: [
+        {
+          name: 'negra modelo',
+          price: 100,
+          quantity: 10,
+        },
+      ],
+      creationDate: new Date('2023-11-06T13:47:05.892Z'),
+      subtotal: 1000,
+      vat: 0.16,
+      total: 1160,
+      isCancel: true,
+      cancellationDate: new Date('2023-11-06T14:12:58.772Z'),
+    };
     expect(component).toBeTruthy();
   });
 });
